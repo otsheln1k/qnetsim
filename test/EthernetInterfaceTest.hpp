40e20f5c7e8cbe1e96527235e7d1464bb0834fd5
Leonid Parfentiev <leo.parfentiev@gmail.com>
2021-04-05 21:03:52 +0300

    Add a large test for activeness reporting

--- test/EthernetInterfaceTest.hpp
+++ test/EthernetInterfaceTest.hpp
@@ -1,7 +1,10 @@
+#include <optional>
+
 #include <QObject>
 #include <QVariant>
 #include <QtTest/QtTest>
 
+#include "MACAddr.hpp"
 #include "EthernetFrame.hpp"
 #include "EthernetInterface.hpp"
 #include "GenericNetworkInterface.hpp"
@@ -208,5 +211,124 @@ private slots:
         QCOMPARE(received, 4);
 
         deleteAll(std::begin(is), std::end(is));
+    }
+
+    void testHalting() {
+        QFETCH(int, n_hops);
+
+        EthernetInterface *is[2] = {
+            new EthernetInterface {},
+            new EthernetInterface {},
+        };
+
+        MACAddr addrs[2] = {
+            0x101010101010,
+            0x202020202020,
+        };
+
+        is[0]->connect(is[1]);
+
+        EthernetFrame f;
+        f.setSrcAddr(addrs[0]);
+        f.setDstAddr(addrs[1]);
+        f.setEtherType(ETHERTYPE_ECTP);
+
+        std::vector<uint8_t> payload {0, 0}; // initial skipCount = 0
+        std::vector<uint8_t> part {2, 0, 0,0,0,0,0,0};
+        std::vector<uint8_t> pfinal {1, 0, 33, 0, 'a', 'b', 'c'};
+        for (int i = 0; i < n_hops; ++i) {
+            addrs[i % 2].write(part.data() + 2);
+            payload.insert(payload.end(), part.begin(), part.end());
+        }
+        payload.insert(payload.end(), pfinal.begin(), pfinal.end());
+
+        size_t pbytes = std::max(payload.size(), ETHERNET_PAYLOAD_MIN);
+        f.payload() = std::move(payload);
+
+        auto iface_by_mac =
+            [&addrs, &is](MACAddr a) -> EthernetInterface *
+            {
+                for (size_t i = 0; i < sizeof(addrs)/sizeof(*addrs); ++i) {
+                    if (addrs[i] == a) {
+                        return is[i];
+                    }
+                }
+                return nullptr;
+            };
+
+        iface_by_mac(f.srcAddr())->sendFrame(f);
+
+        int ns[2] = {0, 0};
+        MACAddr next_src;
+        std::optional<MACAddr> next_dst;
+        auto handler =
+            [&ns, &next_dst, &next_src, &payload, pbytes]
+            (const EthernetFrame *f)
+            {
+                QCOMPARE(f->payload().size(), pbytes);
+
+                payload = f->payload();
+                uint16_t skip = payload[0] | (payload[1] << 8);
+
+                next_src = f->dstAddr();
+                uint8_t func = f->payload()[2 + skip];
+
+                switch (func) {
+                case 1:
+                    next_dst.reset();
+                    break;
+                case 2: {
+                    MACAddr a;
+                    // 2 (skipCount) + skip + 2 (function)
+                    a.read(f->payload().data() + 2 + skip + 2);
+                    next_dst.emplace(a);
+
+                    skip += 8;
+                    payload[0] = skip & 0xFF;
+                    payload[1] = (skip >> 8) & 0xFF;
+                    break;
+                }
+                default:
+                    QFAIL("Invalid ECTP function");
+                    return;
+                }
+                ++ns[func - 1];
+            };
+
+        QObject::connect(is[0], &EthernetInterface::receivedFrame, handler);
+        QObject::connect(is[1], &EthernetInterface::receivedFrame, handler);
+
+        while (stepInterfaces(std::begin(is), std::end(is))) {
+            if (next_dst.has_value()) {
+                EthernetFrame nf;
+                nf.setSrcAddr(next_src);
+                nf.setDstAddr(next_dst.value());
+                nf.setEtherType(ETHERTYPE_ECTP);
+                nf.payload() = std::move(payload);
+
+                auto *iface = iface_by_mac(next_src);
+                QVERIFY(iface != nullptr);
+                iface->sendFrame(nf);
+
+                next_dst.reset();
+            }
+        }
+
+        QCOMPARE(ns[0], 1);
+        QCOMPARE(ns[1], n_hops);
+
+        uint16_t skip = payload[0] | (payload[1] << 8);
+        QVERIFY(!memcmp(pfinal.data(),
+                        payload.data() + 2 + skip,
+                        pfinal.size()));
+
+        deleteAll(std::begin(is), std::end(is));
+    }
+
+    void testHalting_data() {
+        QTest::addColumn<int>("n_hops");
+        QTest::newRow("2 hops") << 2;
+        QTest::newRow("10 hops") << 10;
+        QTest::newRow("100 hops") << 100;
     }
 };
