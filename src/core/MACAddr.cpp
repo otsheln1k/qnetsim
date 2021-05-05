ce8a7a3f04b94aac1042084c338d9f1b9f018701
Leonid Parfentiev <leo.parfentiev@gmail.com>
2021-05-06 02:55:50 +0300

    Make MACAddr a literal type
    Most simple functions are also constexpr

--- src/core/MACAddr.cpp
+++ src/core/MACAddr.cpp
@@ -1,73 +1,5 @@
 #include "MACAddr.hpp"
 
-MACAddr::MACAddr(uint8_t x1, uint8_t x2, uint8_t x3,
-                 uint8_t x4, uint8_t x5, uint8_t x6)
-        :_bs{x1, x2, x3, x4, x5, x6} {}
-
-MACAddr::MACAddr()
-    :MACAddr{0, 0, 0, 0, 0, 0} {}
-
-
-
-MACAddr::MACAddr(uint32_t orgID, uint32_t ifaceID)
-    :MACAddr((orgID >> 16) & 0xFF,
-             (orgID >> 8) & 0xFF,
-             orgID & 0xFF,
-             (ifaceID >> 16) & 0xFF,
-             (ifaceID >> 8) & 0xFF,
-             ifaceID & 0xFF) {}
-
-MACAddr::MACAddr(uint64_t num)
-    :MACAddr((num >> 24) & 0xFFFFFF,
-             num & 0xFFFFFF) {}
-
-bool MACAddr::isMulticast() const
-{
-    return (_bs[0] & 0x01) != 0;
-}
-
-bool MACAddr::isUnicast() const
-{
-    return !isMulticast();
-}
-
-bool MACAddr::isBroadcast() const
-{
-    return asUint64() == BROADCAST_ADDR;
-}
-
-    // Organizationally Unique Identifier (i.e. upper 24 bits are vendor ID)
-bool MACAddr::isUniversal() const
-{
-    return (_bs[0] & 0x02) == 0;
-}
-
-    // Upper 24 bits aren’t a vendor ID
-bool MACAddr::isLocal() const
-{
-    return !isUniversal();
-}
-
-uint32_t MACAddr::organizationID() const
-{
-    return (_bs[0] << 16)
-        |  (_bs[1] << 8)
-        |  (_bs[2]);
-}
-
-uint32_t MACAddr::interfaceID() const
-{
-    return (_bs[3] << 16)
-        |  (_bs[4] << 8)
-        |  (_bs[5]);
-}
-
-uint64_t MACAddr::asUint64() const
-{
-    return ((uint64_t)organizationID() << 24)
-        |  ((uint64_t)interfaceID());
-}
-
 uint8_t *MACAddr::write(uint8_t *dest) const
 {
     memcpy(dest, _bs, sizeof(_bs));
@@ -78,36 +10,6 @@ const uint8_t *MACAddr::read(const uint8_t *src)
 {
     memcpy(_bs, src, sizeof(_bs));
     return src + sizeof(_bs);
-}
-
-bool MACAddr::operator==(const MACAddr &a) const
-{
-    return !memcmp(_bs, a._bs, sizeof(_bs));
-}
-
-bool MACAddr::operator!=(const MACAddr &a) const
-{
-    return !(*this == a);
-}
-
-bool MACAddr::operator<(const MACAddr &a) const
-{
-    return memcmp(_bs, a._bs, sizeof(_bs)) < 0;
-}
-
-bool MACAddr::operator>(const MACAddr &a) const
-{
-    return memcmp(_bs, a._bs, sizeof(_bs)) > 0;
-}
-
-bool MACAddr::operator<=(const MACAddr &a) const
-{
-    return memcmp(_bs, a._bs, sizeof(_bs)) <= 0;
-}
-
-bool MACAddr::operator>=(const MACAddr &a) const
-{
-    return memcmp(_bs, a._bs, sizeof(_bs)) >= 0;
 }
 
 char *MACAddr::display(char *p) const
--- src/core/MACAddr.hpp
+++ src/core/MACAddr.hpp
@@ -14,40 +14,103 @@ private:
 public:
     static const uint64_t BROADCAST_ADDR = 0xFFFFFFFFFFFF;
 
-    MACAddr();
+    constexpr MACAddr(uint8_t x1, uint8_t x2, uint8_t x3,
+                      uint8_t x4, uint8_t x5, uint8_t x6)
+        :_bs{x1, x2, x3, x4, x5, x6} {}
 
-    MACAddr(uint8_t x1, uint8_t x2, uint8_t x3,
-            uint8_t x4, uint8_t x5, uint8_t x6);
+    constexpr MACAddr()
+        :MACAddr{0, 0, 0, 0, 0, 0} {}
 
-    MACAddr(uint32_t orgID, uint32_t ifaceID);
+    constexpr MACAddr(uint32_t orgID, uint32_t ifaceID)
+        :MACAddr((orgID >> 16) & 0xFF,
+                 (orgID >> 8) & 0xFF,
+                 orgID & 0xFF,
+                 (ifaceID >> 16) & 0xFF,
+                 (ifaceID >> 8) & 0xFF,
+                 ifaceID & 0xFF) {}
 
-    MACAddr(uint64_t num);
+    constexpr MACAddr(uint64_t num)
+        :MACAddr((num >> 24) & 0xFFFFFF,
+                 num & 0xFFFFFF) {}
 
-    bool isMulticast() const;
-    bool isUnicast() const;
-    bool isBroadcast() const;
+    constexpr bool isMulticast() const
+    {
+        return (_bs[0] & 0x01) != 0;
+    }
+
+    constexpr bool isUnicast() const
+    {
+        return !isMulticast();
+    }
+
+    constexpr bool isBroadcast() const
+    {
+        return asUint64() == BROADCAST_ADDR;
+    }
 
     // Organizationally Unique Identifier (i.e. upper 24 bits are vendor ID)
-    bool isUniversal() const;
+    constexpr bool isUniversal() const
+    {
+        return (_bs[0] & 0x02) == 0;
+    }
 
     // Upper 24 bits aren’t a vendor ID
-    bool isLocal() const;
+    constexpr bool isLocal() const
+    {
+        return !isUniversal();
+    }
 
-    uint32_t organizationID() const;
-    uint32_t interfaceID() const;
+    constexpr uint32_t organizationID() const
+    {
+        return (_bs[0] << 16)
+            |  (_bs[1] << 8)
+            |  (_bs[2]);
+    }
+    constexpr uint32_t interfaceID() const
+    {
+        return (_bs[3] << 16)
+            |  (_bs[4] << 8)
+            |  (_bs[5]);
+    }
 
-    uint64_t asUint64() const;
+    constexpr uint64_t asUint64() const
+    {
+        return ((uint64_t)organizationID() << 24)
+            |  ((uint64_t)interfaceID());
+    }
 
     uint8_t *write(uint8_t *dest) const;
     const uint8_t *read(const uint8_t *src);
 
-    bool operator==(const MACAddr &a) const;
-    bool operator!=(const MACAddr &a) const;
+    constexpr bool operator==(const MACAddr &a) const
+    {
+        return !memcmp(_bs, a._bs, sizeof(_bs));
+    }
 
-    bool operator<(const MACAddr &a) const;
-    bool operator>(const MACAddr &a) const;
-    bool operator<=(const MACAddr &a) const;
-    bool operator>=(const MACAddr &a) const;
+    constexpr bool operator!=(const MACAddr &a) const
+    {
+        return !(*this == a);
+    }
+
+    constexpr bool operator<(const MACAddr &a) const
+    {
+        return memcmp(_bs, a._bs, sizeof(_bs)) < 0;
+    }
+
+    constexpr bool operator>(const MACAddr &a) const
+    {
+        return memcmp(_bs, a._bs, sizeof(_bs)) > 0;
+    }
+
+    constexpr bool operator<=(const MACAddr &a) const
+    {
+        return memcmp(_bs, a._bs, sizeof(_bs)) <= 0;
+    }
+
+    constexpr bool operator>=(const MACAddr &a) const
+    {
+        return memcmp(_bs, a._bs, sizeof(_bs)) >= 0;
+    }
 
     const char *parse(const char *s);
     char *display(char *p) const;
