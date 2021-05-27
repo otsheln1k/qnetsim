a636e459d80e2a749669a7d483b27e0bc7d8a47e
NikitaSpirin321234 <nikitanikita04062000@gmail.com>
2021-05-27 14:38:30 +0300

    Added serialization of edges

--- src/gui/Serialization.cpp
+++ src/gui/Serialization.cpp
@@ -9,6 +9,8 @@ void Serialization::createSave()
     QDataStream stream(&file);
     if(!file.open(QIODevice::WriteOnly))
         return;
+    std::map<GenericNetworkInterface*, int> iface_indexes{};
+    int conn_count = 0;
 
     std::map<NetworkNode *, NSGraphicsNode *> nodetab = *(interfaceView->getNodetab());
     int countNodes = nodetab.size();
@@ -56,11 +58,36 @@ void Serialization::createSave()
                 stream << mac;
                 qDebug() << "Read Mac: " << mac;
             }
-            //std::map<std::pair<QObject *, QObject *>, QGraphicsLineItem *> edgetab = *(interfaceView->getEdgetab());
-            //qDebug() << "Count lines: " << edgetab.size();
         }
+
+        for(GenericNetworkInterface* iface : *item.first){
+            iface_indexes[iface] = iface_indexes.size();
+            conn_count += iface->connectionsCount();
+        }
+
+        qDebug() << "Start read:";
+        qDebug() << type;
+        qDebug() << pos;
+        qDebug() << size;
+        qDebug() << name;
+        qDebug() << countInterfaces;
         qDebug() << "End read";
     }
+
+    stream << conn_count;
+    for(auto iface_pair : iface_indexes){
+        auto *iface = iface_pair.first;
+        int iface_i = iface_pair.second;
+
+        int c = iface->connectionsCount();
+        for(int i = 0; i < c; ++i){
+            GenericNetworkInterface* other = iface->connectionByIndex(i);
+            int other_i = iface_indexes[other];
+            if(iface_i < other_i)
+                stream << iface_i << other_i;
+        }
+    }
+
     file.close();
 }
 
@@ -76,6 +103,7 @@ void Serialization::loadSave()
     QSize size;
     QString nameStr, *name, type;
     unsigned int countInterfaces;
+    std::vector<GenericNetworkInterface*> ifaces{};
 
     stream >> count;
     for(int i = 0; i < count; i++){
@@ -88,54 +116,39 @@ void Serialization::loadSave()
         qDebug() << type;
         qDebug() << pos;
         qDebug() << size;
-        qDebug() << name;
+        qDebug() << nameStr;
         qDebug() << countInterfaces;
         qDebug() << "End write";
+
+        // adding nodes
 
         QPointF scn = view->mapToScene(pos);
 
         NetworkNode *nd = nullptr;
         NSGraphicsNode *gnode = nullptr;
 
+        PCNode* pnode;
+        HubNode* hnode;
+        SwitchNode* snode;
+
         if(type == "PC"){
-            auto *pnode = new PCNode {};
+            pnode = new PCNode{};
             nd = pnode;
-            for(unsigned int i = 0; i < countInterfaces; i++){
-                auto *iface = new EthernetInterface {};
-                iface->moveToThread(pnode->thread());
-                pnode->addInterface((GenericNetworkInterface *)iface);
-                EthernetDriver* driver = pnode->getDriver(iface);
-                QString macStr;
-                stream >> macStr;
-                qDebug() << "Write Mac: " << macStr;
-                MACAddr mac = driver->address();
-                qDebug() << mac.parseQString(macStr);
-                driver->setAddress(mac);
-            }
             gnode = new NSGraphicsPCNode(view, pnode, scn, size, name);
         }
         else if(type == "HUB"){
-            auto *hnode = new HubNode {};
+            hnode = new HubNode {};
             nd = hnode;
-            for(unsigned int i = 0; i < countInterfaces; i++){
-                auto *iface = new EthernetInterface {};
-                iface->moveToThread(hnode->thread());
-                hnode->addInterface((GenericNetworkInterface *)iface);
-            }
             gnode = new NSGraphicsHubNode(view, hnode, scn, size, name);
         }
         else if(type == "SWITCH"){
-            auto *snode = new SwitchNode {};
+            snode = new SwitchNode {};
             nd = snode;
-            for(unsigned int i = 0; i < countInterfaces; i++){
-                auto *iface = new EthernetInterface {};
-                iface->moveToThread(snode->thread());
-                snode->addInterface((GenericNetworkInterface *)iface);
-            }
             gnode = new NSGraphicsSwitchNode(view, snode, scn, size, name);
         }
 
         interfaceView->moveToThread(nd);
+
         NetworkModel* model = interfaceView->getNetworkModel();
         model->addNode(nd);
 
@@ -152,6 +165,52 @@ void Serialization::loadSave()
 
         std::map<NetworkNode *, NSGraphicsNode *>& nodetab = *(interfaceView->getNodetab());
         nodetab[nd] = gnode;
+
+        // adding interfaces
+
+        if(type == "PC"){
+            for(unsigned int i = 0; i < countInterfaces; i++){
+                auto *iface = new EthernetInterface {};
+                iface->moveToThread(pnode->thread());
+                pnode->addInterface((GenericNetworkInterface *)iface);
+                EthernetDriver* driver = pnode->getDriver(iface);
+                QString macStr;
+                stream >> macStr;
+                qDebug() << "Write Mac: " << macStr;
+                MACAddr mac = driver->address();
+                qDebug() << mac.parseQString(macStr);
+                driver->setAddress(mac);
+
+                ifaces.push_back(iface);
+            }
+        }
+        else if(type == "HUB"){
+            for(unsigned int i = 0; i < countInterfaces; i++){
+                auto *iface = new EthernetInterface {};
+                iface->moveToThread(hnode->thread());
+                hnode->addInterface((GenericNetworkInterface *)iface);
+                ifaces.push_back(iface);
+            }
+
+        }
+        else if(type == "SWITCH"){
+            for(unsigned int i = 0; i < countInterfaces; i++){
+                auto *iface = new EthernetInterface {};
+                iface->moveToThread(snode->thread());
+                snode->addInterface((GenericNetworkInterface *)iface);
+                ifaces.push_back(iface);
+            }
+        }
+    }
+
+    // adding edges
+
+    int conn_count;
+    stream >> conn_count;
+    for(int i = 0; i < (conn_count / 2); ++i){
+        int iface_i, other_i;
+        stream >> iface_i >> other_i;
+        ifaces[iface_i]->connect(ifaces[other_i]);
     }
 
     file.close();
